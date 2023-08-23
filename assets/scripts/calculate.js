2cb284cf485f85966b15727da3d3ac9a61ce4884
dorinengabdoh <ngabdohdorine@gmail.com>
2023-08-23 18:10:12 -0400

    fix linter's error 2

--- assets/scripts/calculate.js
+++ assets/scripts/calculate.js
@@ -7,41 +7,39 @@ let conver = document.getElementById('conver');
 
 function sum(arr) {
   let somme = 0;
-for (let i = 0; i < arr.length; i++){
+  for (let i = 0; i < arr.length; i++){
   somme = somme + arr[i];
-}
+  }
   return somme;
 }
 
 // display
 
 function display() {
- first = first.value.split(',').map(Number);'use strict'
- let first = document.getElementById('cont1');
- let last = document.getElementById('cont2');
- let conver = document.getElementById('conver');
+  first = first.value.split(',').map(Number);'use strict'
+  let first = document.getElementById('cont1');
+  let last = document.getElementById('cont2');
+  let conver = document.getElementById('conver');
 
  // fonction
 
- function sum(arr) {
-   let somme = 0;
- for (let i = 0; i < arr.length; i++) {
-   somme = somme + arr[i];
- }
-   return somme;
- }
+  function sum(arr) {
+     let somme = 0;
+  for (let i = 0; i < arr.length; i++) {
+    somme = somme + arr[i];
+   }
+    return somme;
+  }
  
  // display
  
- function display() {
-  first = first.value.split(',').map(Number);
+  function display() {
+    first = first.value.split(',').map(Number);
+   const all = sum(first);
+   last.value =`${all}`;
+  }
+   conver.addEventListener('click', display);
   const all = sum(first);
   last.value =`${all}`;
- }
- conver.addEventListener('click', display);
- 
- 
- const all = sum(first);
- last.value =`${all}`;
 }
 conver.addEventListener('click', display);
